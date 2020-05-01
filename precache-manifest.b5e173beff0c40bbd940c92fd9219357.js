5312f67cd001335ecd602723f7192e3de05d1554
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-05-01 18:02:23 +0530

    Updates

---
+++ precache-manifest.b5e173beff0c40bbd940c92fd9219357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d0d04c6fd8e95036f25035d0c3a00e",
+    "url": "/currency_rate/index.html"
+  },
+  {
+    "revision": "da0e4bdee05ba79af47c",
+    "url": "/currency_rate/static/css/main.56b6c4d8.chunk.css"
+  },
+  {
+    "revision": "c660f839e013543b4bdb",
+    "url": "/currency_rate/static/js/2.bea846d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency_rate/static/js/2.bea846d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0e4bdee05ba79af47c",
+    "url": "/currency_rate/static/js/main.9084a041.chunk.js"
+  },
+  {
+    "revision": "ac4983579fa7531bb5de",
+    "url": "/currency_rate/static/js/runtime-main.bd84e847.js"
+  },
+  {
+    "revision": "22b9cd7fe9fb38985795c12fe64a4618",
+    "url": "/currency_rate/static/media/q.22b9cd7f.jpg"
+  }
+]);
